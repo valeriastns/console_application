897b0f7f909ed49496f112b258766cf8e7251366
Valeriia <valeriastanis@gmail.com>
2020-11-24 17:18:06 +0100

    automated questions to the user using a loop, added conditions for checking the absence of an empty line, canceling a line, etc.

--- script.js
+++ script.js
@@ -9,12 +9,27 @@ let personalMovieDB = {
     'privat': false,
 };
 
-let a = prompt('What was the last watched movie?'),
-    b = prompt('How did you rate it?'),
-    c = prompt('What was the last watched movie?'),
-    d = prompt('How did you rate it?');
+if (personalMovieDB.count < 10) {
+        console.log('You watched not a lot!');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+        console.log('You are classic movie-watcher!');
+} else if (personalMovieDB.count > 30){
+        console.log('You are a true movie lover!');
+} else {
+    console.log ('Произошла ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt('What was the last watched movie?'),
+    b = prompt('How did you rate it?');
+    if (a != '' && b != '' && a.length < 50 && a != null && b != null) {
+        personalMovieDB.movies[a] = b;
+        console.log('ok');
+    } else {
+        console.log('error');
+        i--
+    }
+}
 
 console.log(personalMovieDB);
