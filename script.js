5876a7a318c0957415bc1e5ef369edac06ff8724
Valeriia <valeriastanis@gmail.com>
2020-11-25 14:50:53 +0100

    added function(called writeYourGenres) with for loop  which gets answers from a user about fav genre of movie and put it to the object. the second function showMyDB is checking the status of DB if it is privat or not and show or not user's DB

--- script.js
+++ script.js
@@ -17,27 +17,48 @@ let personalMovieDB = {
     'privat': false,
 };
 
-if (personalMovieDB.count < 10) {
+function rememberMyfilms() {
+    if (personalMovieDB.count < 10) {
         console.log('You watched not a lot!');
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
         console.log('You are classic movie-watcher!');
-} else if (personalMovieDB.count > 30){
+    } else if (personalMovieDB.count > 30) {
         console.log('You are a true movie lover!');
-} else {
-    console.log ('Произошла ошибка');
-}
-
-
-for (let i = 0; i < 2; i++) {
-    let a = prompt('What was the last watched movie?'),
-    b = prompt('How did you rate it?');
-    if (a != '' && b != '' && a.length < 50 && a != null && b != null) {
-        personalMovieDB.movies[a] = b;
-        console.log('ok');
     } else {
-        console.log('error');
-        i--
+        console.log('Произошла ошибка');
     }
 }
+rememberMyfilms();
 
-console.log(personalMovieDB);
+
+function detectPersonallevel() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('What was the last watched movie?'),
+            b = prompt('How did you rate it?');
+        if (a != '' && b != '' && a.length < 50 && a != null && b != null) {
+            personalMovieDB.movies[a] = b;
+            console.log('ok');
+        } else {
+            console.log('error');
+            i--
+        }
+    }
+}
+detectPersonallevel();
+
+function showMyDB() {
+    if (personalMovieDB.privat != true) {
+        console.log(personalMovieDB);
+    } else {
+        console.log('The information is privat!')
+    }
+}
+showMyDB();
+
+function writeYourGenres(){
+    for (let i = 1; i <=3; i++) {
+        let genre = prompt(`Your favorite genre ${i}`);
+        personalMovieDB.genres[i-1] = genre;
+    }
+}
+writeYourGenres();
